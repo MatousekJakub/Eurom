f1189989229fbc3d92ebf639153fd693a6a17dc4
Jakub Matoušek <jakub.matousek99@gmail.com>
2023-12-29 14:15:21 +0100

    error log

--- EuromFrontend/src/lib/useFetch.ts
+++ EuromFrontend/src/lib/useFetch.ts
@@ -14,34 +14,35 @@ export function useFetch(
 			Authorization: `Bearer ${localStorage.getItem('token')}`,
 		},
 		body: JSON.stringify(body),
-	})
-		.then((res: any) => {
-			if (res.status === 204) {
-				toast('Úspěšně splněno');
-				return;
-			}
-			if (res.status === 400) {
-				toast('400 Nelze provést', { type: 'error' });
-			} else if (res.status === 401)
-				toast('401 Vyžadováno přihlášení', { type: 'error' });
-			else if (res.status === 403)
-				toast('403 Nedostatečné oprávnění', { type: 'error' });
-			else if (res.status === 404) toast('404 Neexistuje', { type: 'error' });
-			else if (res.status === 500)
-				toast('500 Chyba serveru', { type: 'error' });
-			else if (res.status > 400 && res.status < 500)
-				toast(`${res.status} Nespecifikovaná chyba`, { type: 'error' });
-			let outJson;
-			try {
-				outJson = res.json();
-			} catch (e) {
-				console.log('caught');
-				outJson = res.text();
-			}
-			return outJson || '';
-		})
-		.catch((e) => {
-			console.log(e);
-		});
+	}).then((res) => {
+		if (res.status === 204) {
+			toast('Úspěšně splněno');
+			return;
+		}
+		if (res.status === 400) {
+			toast('400 Nelze provést', { type: 'error' });
+		} else if (res.status === 401)
+			toast('401 Vyžadováno přihlášení', { type: 'error' });
+		else if (res.status === 403)
+			toast('403 Nedostatečné oprávnění', { type: 'error' });
+		else if (res.status === 404) toast('404 Neexistuje', { type: 'error' });
+		else if (res.status === 500) toast('500 Chyba serveru', { type: 'error' });
+		else if (res.status > 400 && res.status < 500)
+			toast(`${res.status} Nespecifikovaná chyba`, { type: 'error' });
+		let errorText = 'Unknown error';
+		const outJson = res
+			.clone()
+			.text()
+			.then((text) => {
+				if (text.length > 0) errorText = text;
+			})
+			.then(() =>
+				res.json().catch(() => {
+					console.log(errorText);
+					return;
+				}),
+			);
+		return outJson;
+	});
 	return f;
 }
